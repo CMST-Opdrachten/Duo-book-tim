5bb1f0acd9bb706a354354de96460286cfd02236
Here2peer <timothyravor@live.nl>
2020-09-24 10:29:24 +0200

    Added Asynch configuration.

--- src/main/java/com/example/demo/DuoRestfullBoekenApplication.java
+++ src/main/java/com/example/demo/DuoRestfullBoekenApplication.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAsync
 @RestController
 public class DuoRestfullBoekenApplication {
 
