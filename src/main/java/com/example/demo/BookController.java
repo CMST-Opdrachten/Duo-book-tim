39e379e44a6dc1c617291a06094102cb5f60e6c2
Here2peer <timothyravor@live.nl>
2020-09-24 10:14:44 +0200

    Tiny fixes

--- src/main/java/com/example/demo/BookController.java
+++ src/main/java/com/example/demo/BookController.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
--- src/main/java/com/example/demo/IntegrationTest.java
+++ src/main/java/com/example/demo/IntegrationTest.java
@@ -1,13 +1,11 @@
 package com.example.demo;
 
-import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.math.BigInteger;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.post;
@@ -15,7 +13,7 @@ import static org.hamcrest.CoreMatchers.is;
 
 @Profile("integration-test")
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class IntegrationTest {
 
     @Test
